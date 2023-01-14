57915c6915d616facc2a6d984d6e5d6663ac3b0f
maiiiiiiiiia <hi.maiakulieva@gmail.com>
2023-01-14 13:46:56 +0400

    fix primeLogic.js

--- src/games/primeLogic.js
+++ src/games/primeLogic.js
@@ -4,10 +4,10 @@ import runGame from '../index.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num === 1) {
+  if (num < 2) {
     return false;
   }
-  for (let i = 3; i < num; i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
